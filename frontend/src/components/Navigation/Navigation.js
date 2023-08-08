47be4c97e7a771b31a132ae8223fef9f8dc11fda
Adrienne Tran <adrienne.tran@gmail.com>
2023-08-07 17:24:54 -0700

    change to skybnb

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -12,8 +12,8 @@ function Navigation({ isLoaded }) {
       <ul id='nav-bar'>
         <li className='home'>
           <NavLink exact to="/">
-            <img id='airbnb-icon' src='https://companieslogo.com/img/orig/ABNB-4aaade0f.png' alt='airbnb-icon' width='30px' height='30px' />
-            <span>airbnb</span>
+            <img id='skybnb-icon' src='https://companieslogo.com/img/orig/ABNB-4aaade0f.png' alt='skybnb-icon' width='30px' height='30px' />
+            <span>skybnb</span>
           </NavLink>
         </li>
         <div className='right-side'>
